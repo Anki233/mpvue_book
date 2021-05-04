397b0bec02dac9a2344e966f48e5e46c0d0ee6d8
Anki233 <1750567318@qq.com>
2021-05-04 13:19:51 +0800

    个人中心页面部分完成

--- mpvue_book/src/pages/personal/index.vue
+++ mpvue_book/src/pages/personal/index.vue
@@ -1,13 +1,47 @@
 <template>
-<div>personal组件。。。</div>
+  <div id="personContainer">
+    <div class="header">
+      <img src="/static/imgs/personal/personal.png" alt="">
+      <button>登录</button>
+    </div>
+    <div class="cardList">
+      <div class="card">
+        <span>扫码看书</span>
+        <span class="more"> > </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  #personContainer
+    .header
+      padding 40rpx
+      background #02a774
+      img
+        width 100rpx
+        height 100rpx
+        vertical-align middle
+      button
+        display inline-block
+        height 60rpx
+        line-height 60rpx
+        background rgba(255,255,255,0.5)
+        vertical-align middle
+        margin-left 40rpx
+        max-width 200rpx
+    .cardList
+      width 94%
+      height 60rpx
+      line-height 60rpx
+      margin 10rpx auto
+      border 1rpx solid #eee
+      padding 10rpx
+      .more
+        float right
 </style>
